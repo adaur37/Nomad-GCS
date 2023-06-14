2a8ffeede0cccfa9ff7c323c5bbbaf9d117594e1
Andrew J. Daur <92742783+adaur37@users.noreply.github.com>
2023-06-14 00:07:33 -0400

    Update value_Increase.jsx

--- value_Increase.jsx
+++ value_Increase.jsx
@@ -1,31 +1,35 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
+import TextField from '@material-ui/core/TextField';
 
-export function App(props) {
-  
-  const MyComponent = () => 
-  {
-    const [value, setValue] = useState(10);
-    const inputRef = useRef;
+//MyComponent functional component is created to maintain the inupt value in the state using 'useState' hook with the value of 10
+const MyComponent = () => {
+  const [inputValue, setInputValue] = useState(10);
+  //The 'useRef' hook is used to create a refernce to the input element. The 'inputRef' is assigned to the 'inputRef' prop of the Material-UI 'TextField' component. 
+  const inputRef = useRef(null);
 
-    const handleChange = () =>
-    {
-      const newValue = 
-      parseInt(inputRef.current.value);
-      setValue(newValue);
-    };
+  //The 'handleInputChange' function checks if the new value falls withing the range of 10 to 20. If the condition is met, the 'setInputValue' function is called to update the component's state with the new value. Otherwise, the input value remains unchanged. 
+  const handleInputChange = () => {
+    const newValue = parseInt(inputRef.current.value);
+    //Validation ensures that the user can only change the input value to a number between 10 and 20. Anyother values are ignored. 
+    //The restrictions can be changed to ensure the users input range is enforced.
+    if (newValue >= 10 && newValue <= 20){
+      setInputValue(newValue);
+    }
+  };
 
   return (
-    <div className='App'>
-      <h1>Hello Valued Customer!</h1>
-      <h2>Let's change that 10!</h2>
-      /* This is where we could potentially use the Material UI, but instead we will opt for the <input> tags 
-      */
+    <div>
+    //TextField component from the Material-UI is used to render the input field with a label of "Input Value". the 'defaultValue' prop is set to the 'inputValue' from the component's state, which ensures the input field displays the initial value of 10. The 'inputRef' and 'onChange' props are used to bind the input elements to the 'inputRef' and 'handleInputChange' functions respectively. 
+      <TextField
+        label="Input Value"
+        defaultValue={inputValue}
+        inputRef={inputRef}
+        onChange={handleInputChange}
+      />
     </div>
   );
+};
 
-  }
-  
-}
+export default MyComponent;
 
-// Log to console
-console.log('Hello console')
+//By using 'useRef', the user can change the input value to 20 without triggering any unnecessary re-renders. The value is updated in the components state, allowing you to use it for further processing or pass it to other components as needed. 
